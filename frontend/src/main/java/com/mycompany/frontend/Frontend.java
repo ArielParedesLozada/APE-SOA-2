16c8477b15b443e4228b2b681ef869598ee330f2
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2025-06-04 14:42:48 -0500

    Raiz del proyecto de Java

---
+++ frontend/src/main/java/com/mycompany/frontend/Frontend.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.frontend;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Frontend {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
